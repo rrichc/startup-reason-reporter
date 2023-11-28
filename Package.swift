702fd0a215459db410481873f3d21d5d21f80ded
Richard Chen <richard.chen.dev@gmail.com>
2023-11-27 22:25:10 -0800

    Implement SPM

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "StartupReasonReporter",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "StartupReasonReporter",
+            targets: ["StartupReasonReporter"])
+    ],
+    targets: [
+        .target(
+            name: "StartupReasonReporter",
+            dependencies: [],
+            path: "StartupReasonReporter",
+            cSettings: [
+                .headerSearchPath("."),
+                .define("SWIFT_PACKAGE")
+            ],
+            linkerSettings: [
+                .linkedFramework("UIKit")
+            ]
+        )
+    ]
+)
